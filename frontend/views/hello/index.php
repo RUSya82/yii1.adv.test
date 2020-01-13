fd201d6fb6d9c5e2611c6cb16dcc84b20e67c47f
RUSya82 <mail-spam2012@yandex.ru>
2020-01-13 16:31:50 +0500

    Create Hello COntroller

---
+++ frontend/views/hello/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <?=$content?>
+    </div>
+
+    <div class="body-content">
+
+
+
+    </div>
+</div>
